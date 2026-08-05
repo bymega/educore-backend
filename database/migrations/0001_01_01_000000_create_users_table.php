95b732a77ed73a458785ad8d564c8f4d42d009fb
devricardoo <paulorihcardo18@email.com>
2026-08-05 04:45:42 -0300

    feat(auth): configura Sanctum e controle de permissões com Spatie

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone', 20)->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('status', ['active', 'inactive', 'blocked'])->default('active');
+            $table->timestamp('last_login_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
 
