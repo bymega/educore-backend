ba0e2a5c3983719f3528f92085bdf7fa1954e5cb
devricardoo <paulorihcardo18@email.com>
2026-08-13 03:41:47 -0300

    feat: add dados fakes para responsáveis

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             RolePermissionSeeder::class,
             UserSeeder::class,
             StudentSeeder::class,
+            GuardianSeeder::class,
         ]);
     }
 }
