5535434baab5bf7a0060e783e22316d86b2398a7
devricardoo <paulorihcardo18@email.com>
2026-08-17 02:58:39 -0300

    feat: migration para estrutura academica

---
+++ database/migrations/2026_08_17_023706_create_class_subjects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('class_subjects', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->foreignId('school_class_id')->constrained('school_classes')->restrictOnDelete();
+            $table->foreignId('subject_id')->constrained('subjects')->restrictOnDelete();
+            $table->unsignedTinyInteger('weekly_classes')->nullable();
+            $table->unsignedSmallInteger('workload')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+            $table->unique(['school_class_id', 'subject_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('class_subjects');
+    }
+};
