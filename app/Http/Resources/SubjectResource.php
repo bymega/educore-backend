a4ec127e1f3825a4ad9b23a8077f5e5beb15ab98
devricardoo <paulorihcardo18@email.com>
2026-08-23 22:49:09 -0300

    feat(subjects): adicionar busca e recursos para listagem de disciplinas

---
+++ app/Http/Resources/SubjectResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SubjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'uuid' => $this->uuid,
+            'name' => $this->name,
+            'code' => $this->code,
+            'workload' => $this->workload,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
