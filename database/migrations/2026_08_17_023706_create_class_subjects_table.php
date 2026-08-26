39ee6504cd84bb672c953cc9ab3f1a27ae471bb6
devricardoo <paulorihcardo18@email.com>
2026-08-26 01:04:31 -0300

    fix: altera coluna weekly_classes para nullabe

--- database/migrations/2026_08_17_023706_create_class_subjects_table.php
+++ database/migrations/2026_08_17_023706_create_class_subjects_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->uuid('uuid')->unique();
             $table->foreignId('school_class_id')->constrained('school_classes')->restrictOnDelete();
             $table->foreignId('subject_id')->constrained('subjects')->restrictOnDelete();
-            $table->unsignedTinyInteger('weekly_classes');
+            $table->unsignedTinyInteger('weekly_classes')->nullable();
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
             $table->softDeletes();
