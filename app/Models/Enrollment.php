be59fc4e9a8fb097f34c370cbf9df7cde299d56c
devricardoo <paulorihcardo18@email.com>
2026-08-29 16:36:53 -0300

    feat: adiciona relacionamentos das entidades

---
+++ app/Models/Enrollment.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Enrollment extends Model
+{
+    use HasUuids, HasFactory, SoftDeletes;
+
+    public $fillable = [
+        'student_id',
+        'school_class_id',
+        'enrollment_date',
+        'status',
+    ];
+
+    public function uniqueIds(): array
+    {
+        return ['uuid'];
+    }
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function schoolClass(): BelongsTo
+    {
+        return $this->belongsTo(SchoolClass::class);
+    }
+}
